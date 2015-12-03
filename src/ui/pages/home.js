7be8137bbf5a32025c9fceeba4c0626fbd778ead
Kyle Cesmat <kyle@kylecesmat.com>
2015-12-02 17:40:51 -0700

    wooh2!.

--- src/ui/pages/home.js
+++ src/ui/pages/home.js
@@ -6,7 +6,7 @@ let HomePage = React.createClass({
     render() {
         return (
             <Page name='home'>
-                <h1 className='h2 text-center'>We are getting married!</h1>
+                <h1 className='h2 text-center'>We are getting married!!</h1>
                 <p className='p text-center'>The time has finally come for us to tie the knot and we are so excited to share the day with our friends & family!</p>
                 <p className='p text-center'>Join us March 19th at 5:45pm for our ceremony, followed by a reception.</p>
             </Page>
