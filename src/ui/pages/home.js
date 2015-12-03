c5657d0ea39061d71e7ab137b94a33a1c2152ca8
Kyle Cesmat <kyle@kylecesmat.com>
2015-12-02 19:10:35 -0700

    make the things hip.

--- src/ui/pages/home.js
+++ src/ui/pages/home.js
@@ -7,8 +7,8 @@ let HomePage = React.createClass({
         return (
             <Page name='home'>
                 <h1 className='h2 text-center'>We are getting married!</h1>
-                <p className='p text-center'>The time has finally come for us to tie the knot and we are so excited to share the day with our friends & family!</p>
-                <p className='p text-center'>Join us March 19th at 5:45pm for our ceremony, followed by a reception & cash bar. Dress is semi-formal, we'll have plenty of food & drink that will be followed by an ice cream social.</p>
+                <p className='p super-hip text-center'>The time has finally come for us to tie the knot and we are so excited to share the day with our friends & family!</p>
+                <p className='p super-hip text-center'>Join us March 19th at 5:45pm for our ceremony, followed by a reception & cash bar. Dress is semi-formal, we'll have plenty of food & drink that will be followed by an ice cream social.</p>
             </Page>
         );
     }
